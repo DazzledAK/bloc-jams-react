82557c4134bd6fa93dc34e3b150c32213408e8ef
Andriy <40002801+DazzledAK@users.noreply.github.com>
2018-07-27 19:21:35 -0500

    Took out code written twice

--- src/components/Library.js
+++ src/components/Library.js
@@ -15,7 +15,6 @@ class Library extends Component {
           this.state.albums.map( (album, index) =>
             <Link id="main-link" to={`/album/${album.slug}`} key={index}>
               <img id="images-song" src="https://raw.githubusercontent.com/DazzledAK/bloc-jams-react/gh-pages/assets/images/album_covers/01.jpg" alt={album.title} />
-              <img id="images-song" src="https://raw.githubusercontent.com/DazzledAK/bloc-jams-react/gh-pages/assets/images/album_covers/02.jpg" alt={album.title} />
               <div className="link-song">{album.title}</div>
               <div className="link-song">{album.artist}</div>
               <div className="link-song">{album.songs.length} songs</div>
