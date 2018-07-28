ec858181ff5d7ee302eab740b4bc5ad74bbb196c
DazzledAK <youwatchme22@gmail.com>
2018-07-27 19:09:14 -0500

    fixing imgs

--- src/components/Library.js
+++ src/components/Library.js
@@ -14,7 +14,7 @@ class Library extends Component {
         {
           this.state.albums.map( (album, index) =>
             <Link id="main-link" to={`/album/${album.slug}`} key={index}>
-              <img id="images-song" src={album.albumCover} alt={album.title} />
+              <img id="images-song" src="https://raw.githubusercontent.com/DazzledAK/bloc-jams-react/gh-pages/assets/images/album_covers/01.jpg" alt={album.title} />
               <div className="link-song">{album.title}</div>
               <div className="link-song">{album.artist}</div>
               <div className="link-song">{album.songs.length} songs</div>
